81da07130fd9ca2ef3d33acb030e8a31e28ae56d
Noor-Education <110144611+Noor-Education@users.noreply.github.com>
2023-06-19 11:54:18 +0930

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+let mySprite = sprites.create(img`
+    . . . . . . . c c c . . . . . . 
+    . . . . . . c b 5 c . . . . . . 
+    . . . . c c c 5 5 c c c . . . . 
+    . . c c b c 5 5 5 5 c c c c . . 
+    . c b b 5 b 5 5 5 5 b 5 b b c . 
+    . c b 5 5 b b 5 5 b b 5 5 b c . 
+    . . f 5 5 5 b b b b 5 5 5 c . . 
+    . . f f 5 5 5 5 5 5 5 5 f f . . 
+    . . f f f b f e e f b f f f . . 
+    . . f f f 1 f b b f 1 f f f . . 
+    . . . f f b b b b b b f f . . . 
+    . . . e e f e e e e f e e . . . 
+    . . e b c b 5 b b 5 b f b e . . 
+    . . e e f 5 5 5 5 5 5 f e e . . 
+    . . . . c b 5 5 5 5 b c . . . . 
+    . . . . . f f f f f f . . . . . 
+    `, SpriteKind.Player)
+let number = game.askForNumber("Enter a number")
+if (number % 2 == 0) {
+    mySprite.sayText("It is even")
+} else {
+    mySprite.sayText("It is odd")
+}
